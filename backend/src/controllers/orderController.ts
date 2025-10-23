ee8a8cbab2cb10c8f980d3942089467594db2c6e
Liedsonfsa <fabricoliedson@gmail.com>
2025-10-22 23:41:21 -0300

    fix(backend/orders): corrigindo o erro na rota de listar pedidos

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -10,11 +10,11 @@ import { OrderService } from '../services/orderService';
  */
 export const getUserOrders = async (req: Request, res: Response) => {
     try {
-        const orders = OrderService.getUserOrders(req.body.usuarioId);
+        const orders = await OrderService.getUserOrders(req.body.usuarioId);
 
         res.status(200).json({
             success: true,
-            orders: orders
+            orders
         })
     } catch (error) {
         return res.status(500).json({
